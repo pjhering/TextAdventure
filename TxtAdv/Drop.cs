268c32f2aeca7872caf08e493691e29f2197abdb
Pete Hering <pjhering@gmail.com>
2018-12-17 06:28:01 -0500

    alpha version of drop

--- TxtAdv/Drop.cs
+++ TxtAdv/Drop.cs
@@ -1,11 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace TxtAdv
 {
     public class Drop : Command
     {
         string Command.Execute(Model model, string[] args)
         {
-            return "not implemented yet";
+            if(args.Length < 2)
+            {
+                return "drop what?";
+            }
+
+            StringBuilder sb = new StringBuilder("you dropped:\n");
+            HashSet<Item> dropped = new HashSet<Item>();
+            Player p = model.Player;
+            Room r = model.Rooms[p.Location];
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                foreach(Item it in p.Inventory)
+                {
+                    if(it.Name.Equals(args[i]))
+                    {
+                        dropped.Add(it);
+                        sb.Append("  ")
+                            .Append(it.Name)
+                            .Append("\n");
+                    }
+                }
+            }
+
+            if(dropped.Count == 0)
+            {
+                sb.Append("  nothing!\n");
+            }
+
+            foreach(Item it in dropped)
+            {
+                p.Inventory.Remove(it);
+                r.Contents.Add(it);
+            }
+
+            return sb.ToString();
         }
     }
 }
